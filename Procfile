7c119906dacbfa98e6fa6d95323071eb17e41d91
Vikash Rathor <rathorvikash010@gmail.com>
2025-11-27 23:36:11 +0530

    remove unneccessary files

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn samachar.wsgi --log-file -
--- bookmarks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.bookmark_list_view, name='bookmark_list'),
-    path('add/', views.bookmark_create_view, name='bookmark_create'),
-    path('<int:id>/edit/', views.bookmark_update_view, name='bookmark_update'),
-    path('<int:id>/delete/', views.bookmark_delete_view, name='bookmark_delete'),
-]
